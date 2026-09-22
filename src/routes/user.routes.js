46beed2a3fc002982621ff6d443739a6ec3cd208
Pankaj <pankaj929063@gmail.com>
2026-09-22 18:46:32 +0530

    add user profile management features including password change, account update, and user channel profile retrieval

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser,logoutUser,refreshAccessToken,register } from "../controllers/user.controller.js";
+import {
+    register,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+} from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
@@ -24,7 +36,50 @@ router.route("/login").post(loginUser)
 //secure route
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+// Change password
+router.route("/change-password").post(
+    verifyJWT,
+    changeCurrentPassword
+);
 
+
+// Current user
+router.route("/current-user").get(
+    verifyJWT,
+    getCurrentUser
+);
+
+router.route("/update-account").patch(
+    verifyJWT,
+    updateAccountDetails
+);
+
+
+// Update avatar
+router.route("/avatar").patch(
+    verifyJWT,
+    upload.single("avatar"),
+    updateUserAvatar
+);
+
+// Update cover image
+router.route("/cover-image").patch(
+    verifyJWT,
+    upload.single("coverImage"),
+    updateUserCoverImage
+);
+
+// User channel profile
+router.route("/c/:username").get(
+    verifyJWT,
+    getUserChannelProfile
+);
+
+// Watch history
+router.route("/history").get(
+    verifyJWT,
+    getWatchHistory
+);
 
 
 export default router;
